e714bc4958fa0ec899b78ec651999e12550131d6
nulgath <justinemedallon4@gmail.com>
2022-10-04 15:56:53 +0800

    updated

--- dk/src/app/app-routing.module.ts
+++ dk/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomepageComponent } from './comps/homepage/homepage.component';
+import { OverviewComponent } from './comps/overview/overview.component';
+
+const routes: Routes = [
+  {
+    path:"homepage", component:HomepageComponent
+  },
+  {
+    path:"overview", component:OverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
